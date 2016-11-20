047e79f3d15157569134b97ebeb168cbfdae53a3
Jorge Galat <jrgglt@gmail.com>
2016-11-20 20:29:27 -0300

    Updates on statics - Game's display

--- static/js/build.js
+++ static/js/build.js
@@ -1,20 +1,44 @@
+function buildGame(where, game) {
+
+  var data = game.player1.username + ' vs ';
+
+  if (game.player2) {
+    data += game.player2.username;
+    data += ' <span class="activable watch" game="' + game.game_id + '">Watch!<span>';
+  } else {
+    data += '<span class="activable join" game="' + game.game_id + '">Join!</span>';
+  }
+
+  where.append('<li> Game ' + game.game_id + ': ' + data + '</li>');
+
+};
+
 function buildLobby(where) {
+
   where.html('<div class="lobby"></div>');
 
   var lobby = where.find(".lobby");
 
   getGames(function (games) {
+
     lobby.append('<ul>');
 
     games.forEach(function (game) {
-      var adversaries = game.player1.username;
 
-      if (game.player2)
-        adversaries += ' vs ' + game.player2.username;
+      buildGame(lobby, game);
 
-      lobby.append('<li> Game ' + game.game_id + ': ' + adversaries + '</li>')
     });
 
     lobby.append('</ul>');
+
+    lobby.find(".join").click(function () {
+      /* something */
+    });
+
+    lobby.find(".watch").click(function () {
+      /* something */
+    });
+
   });
-}
+
+};
